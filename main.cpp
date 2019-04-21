5169e638f60c86671a4b2b570b23634ffb4b104b
hanserx <49519749+hanserx@users.noreply.github.com>
2019-04-21 20:06:21 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,101 @@
-#include<stdio.h>
+  #include<stdio.h>
+	     #include<stdlib.h>
+ 	    #define N sizeof(Node)
 
-int Maxsum(int a[], int n)
-{
-	int sum, Maxsum;
-	sum = Maxsum = 0;
-	int i, j;
-	for (i=0; i<n; i++)
-	{
-		sum = 0;
-		for (j=i; j<n; j++)
-		{
-			sum+=a[j];
-			if (sum > Maxsum)
-				Maxsum=sum;
-		}
-	}
-	return Maxsum;
-}
-int main()
-{
-	int a[100], number;
-	scanf("%d", &number);
-	for (int i=0; i<number; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-	printf("%d", Maxsum(a, number));
-	return 0;
-}
+ 	    struct Node
+ 	    {
+	         int date;
+ 	        Node*next;
+ 	    };
+
+ 	     Node*create()
+	     {
+ 	         Node*head,*p,*pre;
+ 	        int n;
+ 	         head=p=pre=NULL;
+ 	         n=0;
+
+ 	         scanf("%d",&n);
+ 	         while(n!=-1)
+ 	        {
+ 	             p=(Node*)malloc(N);
+ 	             if(head==NULL)
+	            {
+ 	                 head=p;
+ 	            }
+	             else
+	             {
+	                 pre->next=p;
+	        }
+ 	            p->date=n;
+	             p->next=NULL;
+	             pre=p;
+ 	             scanf("%d",&n);
+ 	        }
+	         return head;
+ 	     }
+
+ 	    Node* fun(Node* head)
+ 	     {
+	         Node* p,*pre;
+ 	        int a,b;
+ 	         p=pre=head;
+ 	         a=b=0;
+
+	        scanf("%d%d",&a,&b);
+ 	         while(p)
+ 	    {
+ 	             if(p->date>a&&p->date<b)
+ 	            {
+	                 if(p==head)
+	                {
+	                    head=pre=p->next;
+	            }
+                 else
+ 	                 {
+ 	                    pre->next=p->next;
+ 	                }
+ 	                free(p);
+
+	                if(pre!=NULL)
+	                   {
+	                      p=pre->next;
+ 	                   }
+	                 else
+ 	                    {
+ 	                        break;
+ 	                    }
+	             }
+
+ 	            else
+	             {
+	                pre=p;
+ 	                p=p->next;
+ 	            }
+
+	        }
+
+ 	         return head;
+ 	     }
+
+ 	    void display(Node* head)
+ 	     {
+	        Node* p;
+ 	         p=head;
+
+ 	         while(p)
+	        {
+ 	            printf("%d ",p->date);
+ 	            p=p->next;
+ 	        }
+ 	    }
+
+	 int main()
+ 	{
+ 	         Node* p;
+ 	         p=create();
+ 	         p=fun(p);
+ 	        display(p);
+
+ 	         return 0;
+ 	}
